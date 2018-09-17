15a8727b7d3591ad14ed0ce45f13ec8ec150558f
Adam <adamg@ruist.com>
2018-09-17 14:59:17 -0700

    redux intro project

---
+++ redux-intro/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Counter from './counter';
+
+const App = () => (
+  <div>
+    <Counter />
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
